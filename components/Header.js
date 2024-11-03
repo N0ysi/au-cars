b077eefe56439101f4e0251d7bcc41576782c10b
N0ysi <iliupov200312@gmail.com>
2024-11-03 10:24:04 +0100

    improved @media in global.css to better ux on smaller screens

--- components/Header.js
+++ components/Header.js
@@ -12,22 +12,35 @@ export default function Header() {
   return (
     <header>
       <div className="container">
-        <div className='head'>
-          <div className='navigation'>
-            <Link href="/" className="logo">
-              <img src="/img/logo.svg" alt="logo" />
-            </Link>
 
+        <div className='navigation'>
+          <Link href="/" className="logo">
+            <img src="/img/logo.svg" alt="logo" />
+          </Link>
 
-            <ul>
-              <li><a href="#section-Home">Home</a></li>
-              <li><a href="#section-WhatWeDo">What we do</a></li>
-              <li><a href="#section-NewCars">New interesting cars</a></li>
-              <li><a href="#section-AboutUs">About us</a></li>
-              <li><a href="#section-Social">Socials</a></li>
-            </ul>
-
+          <div className="hamburgerDiv">
+            <label className="hamburger-menu">
+              <input type="checkbox" />
+            </label>
+            <aside className="sidebar">
+              <nav>
+                <a href="#section-Home">Home</a>
+                <a href="#section-WhatWeDo">What we do</a>
+                <a href="#section-NewCars">New interesting cars</a>
+                <a href="#section-AboutUs">About us</a>
+                <a href="#section-Social">Socials</a>
+              </nav>
+            </aside>
           </div>
+
+          <ul>
+            <li><a href="#section-Home">Home</a></li>
+            <li><a href="#section-WhatWeDo">What we do</a></li>
+            <li><a href="#section-NewCars">New interesting cars</a></li>
+            <li><a href="#section-AboutUs">About us</a></li>
+            <li><a href="#section-Social">Socials</a></li>
+          </ul>
+
           <div className='header-buttons'>
             {!user ? (
               <Link href="/login">
@@ -42,20 +55,6 @@ export default function Header() {
               </div>
             )}
             <button className="btn">Talk to an Expert</button>
-          </div>
-          <div className="hamburgerDiv">
-            <label className="hamburger-menu">
-              <input type="checkbox" />
-            </label>
-            <aside className="sidebar">
-              <nav>
-                <a href="#section-Home">Home</a>
-                <a href="#section-What we do">What we do</a>
-                <a href="#section-New interesting cars">New interesting cars</a>
-                <a href="#section-About us">About us</a>
-                <a href="#section-Social">Socials</a>
-              </nav>
-            </aside>
           </div>
         </div>
       </div>
